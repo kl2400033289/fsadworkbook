fce027e9ffbe074459d41cf794556e16ca04a617
kl2400033289 <2400033289@kluniversity.in>
2026-03-05 10:48:03 +0530

    Delete exp5-SKILL directory

--- exp5-SKILL/src/main/java/com/klu/config/Appconfig.java
+++
@@ -1,9 +0,0 @@
-package com.klu.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.klu")
-public class Appconfig {
-}
